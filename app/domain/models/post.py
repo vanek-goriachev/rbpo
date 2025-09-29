3ed8cf364cbc59e7e4910b015f9cfc25c4455fc5
isegoryachev <isegoryachev@avito.ru>
2025-09-29 22:51:30 +0300

    Исправления в доменной модели

--- app/domain/models/post.py
+++ app/domain/models/post.py
@@ -3,11 +3,17 @@ from datetime import datetime as Datetime
 
 from app.domain.models.tag import Tag
 
+POST_STATUS_DRAFT = "draft"
+POST_STATUS_ON_MODERATION = "on_moderation"
+POST_STATUS_PUBLIC = "public"
+POST_STATUS_ARCHIVE = "archive"
+
 
 class Post:
     id_: uuid.UUID
     title: str
-    text: str
+    body: str
+    status: str
     created_at: Datetime
     updated_at: Datetime
     tags: list[Tag]
